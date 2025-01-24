727ec5dca14c2549829316dbaf4167e532ba9615
백아현 <baeg-ahyeon@baeg-ahyeon-ui-MacBookAir.local>
2025-01-24 09:14:21 +0900

    viteful

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const HomePage = () => {
+  const testFetch = async () => {
+    const result = await fetch("/api/test")
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
+    console.log(result);
+  };
+  useEffect(() => {
+    testFetch();
+  }, []);
   return (
     <>
       <h2>Home Page</h2>
